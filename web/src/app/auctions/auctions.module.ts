79709c9f0b5c697aee13e99685efa9ba86b797e0
Mamoud Srour <msrour@mum.edu>
2020-02-01 18:36:43 -0600

    Create pages layout, design, routing

--- web/src/app/auctions/auctions.module.ts
+++ web/src/app/auctions/auctions.module.ts
@@ -7,15 +7,20 @@ import { BidComponent } from './bid/bid.component';
 import { CommentsComponent } from './comments/comments.component';
 import { RatingComponent } from './rating/rating.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from "@angular/router";
 
-
+const routes = [
+  {path: ':auctionId', component: BidComponent, pathMatch: 'full' },
+  {path: '', component: CreateComponent, pathMatch: 'full' }
+  ];
 
 @NgModule({
   declarations: [CreateComponent, ViewComponent, ListComponent, BidComponent, CommentsComponent, RatingComponent],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
   ] ,
   exports:[
     CreateComponent, ViewComponent, ListComponent, BidComponent, CommentsComponent, RatingComponent
