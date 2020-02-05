7b0aea97a5741df667d50ce4895d8251b0109d21
Mamoud Srour <msrour@mum.edu>
2020-02-04 21:53:25 -0600

    Allow png images to be uploaded

--- server/middelwares/uploader.js
+++ server/middelwares/uploader.js
@@ -1,19 +1,20 @@
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'server/assets/images')
+        cb(null, global.gConfig.storageDir)
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + ".jpg")
+        cb(null, Date.now() + path.extname(file.originalname))
     }
 });
 
 module.exports = multer({
     storage: storage,
     fileFilter: (req, file, cb) => {
-        if (file.mimetype !== 'image/jpeg') {
-            return cb({ status: 400,message: 'invalid file, only .jpg images are allowed'}, false);
+        if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+            return cb({ status: 400,message: 'invalid file, only .jpg or png images are allowed'}, false);
         }
         cb(null, true);
     },
